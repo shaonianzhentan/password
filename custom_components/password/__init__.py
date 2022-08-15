bb8573afc83a39dc7eb92b1e496b0baa58d6f512
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2022-08-15 20:08:30 +0800

    update

--- custom_components/password/__init__.py
+++ custom_components/password/__init__.py
@@ -1,6 +1,7 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
+from hass.components import frontend
 
 from .const import DOMAIN, WWW, VERSION
 
@@ -9,13 +10,8 @@ CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.http.register_view(HttpView)
     hass.http.register_static_path(WWW, hass.config.path(f"custom_components/{DOMAIN}/www"), False)
-    hass.components.frontend.async_register_built_in_panel(
-                        "iframe",
-                        "书签",
-                        "mdi:bookmark",
-                        DOMAIN,
-                        { "url": f"{WWW}/index.html?ver={VERSION}" },
-                        require_admin=entry.options.get('require_admin', False))
+    
+    frontend.async_register_built_in_panel(hass, "panel-password", "panel-password", "mdi:book-lock-outline")
     entry.async_on_unload(entry.add_update_listener(update_listener))
     return True
 
@@ -25,5 +21,5 @@ async def update_listener(hass, entry):
     await async_setup_entry(hass, entry)
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.components.frontend.async_remove_panel(DOMAIN)
+    frontend.async_remove_panel(DOMAIN)
     return True
