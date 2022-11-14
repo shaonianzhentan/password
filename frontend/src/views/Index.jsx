a4d675024fd637d8a3f7199195d8f9b71b2af627
shaonianzhentan <ilovevar@gmail.com>
2022-11-14 20:56:43 +0800

    update

--- frontend/src/views/Index.jsx
+++ frontend/src/views/Index.jsx
@@ -8,6 +8,7 @@ import ShowPassword from '../components/ShowPassword';
 
 export default function App() {
 
+    const [categoryList, setCategoryList] = useState([])
     const [source, setSource] = useState([])
     const [list, setList] = useState([])
 
@@ -18,6 +19,7 @@ export default function App() {
             const { data } = res
             setSource(data)
             setList(data)
+            setCategoryList(Array.from(new Set(data.map(ele => ele.category))))
         })
     }, [])
 
@@ -52,9 +54,7 @@ export default function App() {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>1还没做</a></li>
-                            <li><a>2后面做</a></li>
-                            <li><a>3这是分类筛选</a></li>
+                            {categoryList.map(ele => <li onClick={() => search(ele)}><a>{ele}</a></li>)}
                         </ul>
                     </div>
                     <div className="form-control w-full">
