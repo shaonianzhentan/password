39e4a164c3615aeee9db6f1c799768c792c71707
shaonianzhentan <ilovevar@gmail.com>
2022-11-25 14:29:39 +0800

    update

--- custom_components/password/__init__.py
+++ custom_components/password/__init__.py
@@ -5,6 +5,11 @@ import homeassistant.helpers.config_validation as cv
 from .http import HttpView
 from .manifest import manifest
 from .const import DOMAIN
+from .const import MAC_KEY
+from .storage import StorageData
+from .EncryptHelper import EncryptHelper
+
+sd = StorageData('password')
 
 CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
@@ -15,7 +20,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     require_admin = config.get('require_admin', True)
 
     # 保存密钥
-    hass.data.setdefault(f'{DOMAIN}-key', config.get('key'))
+    server_key = config.get('key')
+    hass.data.setdefault(f'{DOMAIN}-key', server_key)
 
     hass.http.register_view(HttpView)
     url = f'/{url_path}-www'
@@ -26,6 +32,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             {"url": f"{url}/index.html?v={manifest.version}"},
             require_admin=require_admin,
         )
+    # 判断是否异常
+    _list = sd.load()
+    if len(_list) > 0:
+        item = _list[0]
+        helper = EncryptHelper(server_key, MAC_KEY)
+        try:
+            helper.Decrypt(item['key'])
+        except Exception as ex:
+            print(ex)
+            await hass.services.async_call('persistent_notification', 'create', {
+                'title': manifest.name,
+                'message': '当前密码文件无法解密，请在集成选项中输入MAC值更新密码文件'
+            })
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
