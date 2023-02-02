0d9b9f8d538956bb2c19758a1729f2b45c28d501
shaonianzhentan <ilovevar@gmail.com>
2023-02-02 21:15:40 +0800

    使用HomeAssistant的UUID

--- custom_components/password/const.py
+++ custom_components/password/const.py
@@ -1,5 +1,7 @@
-import uuid
+import os
 from .manifest import manifest
+from homeassistant.util.json import load_json
+from homeassistant.helpers.storage import STORAGE_DIR
 
 DOMAIN = manifest.domain
-MAC_KEY = uuid.UUID(int=uuid.getnode()).hex[-12:]
+MAC_KEY = load_json(os.path.abspath(f'{STORAGE_DIR}/core.uuid'))['data']['uuid']
