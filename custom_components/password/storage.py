257aa82e3f0388138655f291bfc3095ba498592a
shaonianzhentan <ilovevar@gmail.com>
2024-08-09 09:06:42 +0800

    兼容新版本

--- custom_components/password/storage.py
+++ custom_components/password/storage.py
@@ -1,5 +1,6 @@
 import os
-from homeassistant.util.json import load_json, save_json
+from homeassistant.util.json import load_json
+from homeassistant.helpers.json import save_json
 from homeassistant.helpers.storage import STORAGE_DIR
 
 class StorageData():
